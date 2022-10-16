d88f174eb8c8a44a92c2e2efba7b72429eb6da73
Luis Angel Perez Melendez <angelmelen88@gmail.com>
2022-10-16 16:34:10 -0500

    agregar eventos de react onClick

--- src/CreateTodoButton/CreateTodoButton.js
+++ src/CreateTodoButton/CreateTodoButton.js
@@ -2,7 +2,16 @@ import React from 'react';
 import "./CreateTodoButton.css";
 
 export function CreateTodoButtom(props){
+    const onClickButton = (msg) => {
+        alert(msg);
+    };
     return(
-        <button  className="CreateTodoButton">+</button>
-    );
+            // {/* ✅ */}
+            <button
+            className="CreateTodoButton"
+            onClick={() => onClickButton('Aquí se debería abrir el modal')}
+            >
+            +
+        </button>
+    );   
 }
--- src/TodoSearch/TodoSearch.js
+++ src/TodoSearch/TodoSearch.js
@@ -2,9 +2,14 @@ import React from 'react';
 import "./TodoSearch.css";
 
 export function TodoSearch(){
+    const onSearchValueChange = (event) => {
+        console.log(event.target.value);
+      };
     return(
         <input 
-        className="TodoSearch"
-        placeholder="Cebolla" />
+            className="TodoSearch"
+            placeholder="Cebolla" 
+            onChange={onSearchValueChange}
+        />
     );
 }
