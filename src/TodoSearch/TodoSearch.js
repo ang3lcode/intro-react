101cb827ebd6759829e972196b29b56a49f8508c
Luis Angel Perez Melendez <angelmelen88@gmail.com>
2022-10-16 17:51:57 -0500

    manejo del estado, agregar un estado

--- src/TodoSearch/TodoSearch.js
+++ src/TodoSearch/TodoSearch.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import "./TodoSearch.css";
 
+
 export function TodoSearch(){
+    const [searchValue, setSearchValue] = React.useState('');
+
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
-      };
-    return(
+        setSearchValue(event.target.value);
+    };
+    return[
         <input 
             className="TodoSearch"
             placeholder="Cebolla" 
+            value={searchValue}
             onChange={onSearchValueChange}
-        />
-    );
+        />,
+        <p>{searchValue}</p>   
+    ];
 }
