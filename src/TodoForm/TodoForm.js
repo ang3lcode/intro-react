1eb8b0e1920bf012b0e40b6c853324757b8d1341
Luis Angel Perez Melendez <angelmelen88@gmail.com>
2022-10-17 17:45:42 -0500

    todoForm

---
+++ src/TodoForm/TodoForm.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { TodoContext } from '../TodoContext/TodoContext';
+import './TodoForm.css';
+
+
+export function TodoForm() {
+  const [newTodoValue, setNewTodoValue] = React.useState("");
+
+  const { addTodos, setOpenModal } = React.useContext(TodoContext);
+
+  const onChange = (event) => {
+    setNewTodoValue(event.target.value);
+    //TODO
+  };
+
+  const onCancel = () => {
+    setOpenModal(false);
+    //TODO
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    addTodos(newTodoValue);
+    setOpenModal(false);
+  };
+  return (
+    <form className="form-container" onSubmit={onSubmit}>
+      <label>Registra un nuevo ToDo</label>
+      <textarea
+        rows="15"
+        cols="30"
+        value={newTodoValue}
+        onChange={onChange}
+        placeholder="Cortar la cebolla para el almuerzo"
+      />
+      <div className="conatiner-buttons">
+        <button className="btn btn-danger" type="button" onClick={onCancel}>
+          Cancelar
+        </button>
+        <button className="btn btn-add" type="submit">
+          Añadir
+        </button>
+      </div>
+    </form>
+  );
+}
